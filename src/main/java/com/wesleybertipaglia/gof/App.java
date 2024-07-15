00c2e26dfe5519a8f58e258a549c951e2b264b1a
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-07-15 12:13:13 -0300

    feat: add singleton

--- src/main/java/com/wesleybertipaglia/gof/App.java
+++ src/main/java/com/wesleybertipaglia/gof/App.java
@@ -1,8 +1,12 @@
 package com.wesleybertipaglia.gof;
 
+import com.wesleybertipaglia.gof.create.Singleton;
+
 public class App {
 
 	public static void main(String[] args) {
+		Singleton singleton = Singleton.getInstance();
+		System.out.println(singleton);
 	}
 
 }
